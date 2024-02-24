193eca4fdabd0945cde5d6276dc4d873af0d2d5b
yeedaKing <143111017+yeedaKing@users.noreply.github.com>
2024-02-24 15:38:03 -0700

    Add files via upload

---
+++ List.cpp
@@ -0,0 +1,108 @@
+//
+//  List.cpp
+//  DoublyLinkedList
+//
+//  Created by Madhuri Debnath on 9/24/20.
+//
+
+#include<iostream>
+#include "List.h"
+using namespace std;
+
+void List::insert(int value) {   
+    Node* temp = new Node(value);
+    if (!head->getNextPtr) {
+        headPtr->setNextPtr(temp);
+    
+    } else {
+        Node* prev = tailPtr->getPrevPtr();
+        prev->setNextPtr(temp);
+        temp->setPrevPtr(prev);
+    }
+    tailPtr->setPrevPtr(temp);
+}
+
+void List::insertAtIndex(int data, int index) {
+    if (!headPtr->getNextPtr()) {
+        if (index) {
+            return;
+        }
+        Node* temp = new Node(data);
+        headPtr->setNextPtr(temp);
+        prevPtr->setPrevPtr(temp);
+        return;
+    }
+    Node* curr = headPtr->getNextPtr();
+    Node* prev = headPtr->getPrevPtr();
+    while (curr && index) {
+        prev = curr;
+        curr = curr->getNextPtr();
+        index--;
+    }
+    if (index) {
+        return;
+    }
+    Node* temp = new Node(data, curr, prev);
+    prev->setNextPtr(temp);
+    curr->setPrevPtr(temp);
+}
+void List::deleteAtIndex(int index) {
+    if (!headPtr->getNextPtr()) {
+        return;
+    }
+    Node* curr = headPtr->getNextPtr();
+    Node* prev = headPtr;
+    while (curr && index) {
+        prev = curr;
+        curr = curr->getNextPtr();
+        index--;
+    }
+    if (index) {
+        return;
+    }
+    prev->setNextPtr(curr->getNextPtr());
+    prev->getNextPtr()->setPrevPtr(prev);
+}
+
+void List::readItem(int index) {
+    Node* curr = headPtr->getNextPtr();
+    while (curr && index) {
+        curr = curr->getNextPtr();
+        index--;
+    }
+    return curr;
+}
+
+void List::reverseList() {
+    Node* first = headPtr->getNextPtr();
+    Node* last = tailPtr->getPrevPtr();
+    while (last != first && last && first) {
+        Node* temp1 = first->getPrevPtr();
+        Node* temp2 = last->getPrevPtr();
+        temp1->setNextPtr(last);
+        temp2->setNextPtr(first);
+        first->getNextPtr()->setPrevPtr(last);
+        if (last->getNextPtr()) {
+            last->getNextPtr()->setPrevPtr(first);
+        }
+        first = first->getNextPtr();
+        last = last->getPrevPtr();
+    }
+}
+
+void List::printForward() {
+    Node* curr = headPtr->getNextPtr();
+    while (curr) {
+        cout << curr->getData() << endl;
+        curr = curr->getNextPtr();
+    }
+}
+
+void List::printBackward() {
+    Node* curr = tailPtr->getPrevPtr();
+    while (curr) {
+        cout << curr->getData() << endl;
+        curr = curr->getPrevPtr();
+    }
+}
+
---
+++ makefile
@@ -0,0 +1,24 @@
+#Place this makefile in the same directory as your
+#all .cpp and .h files
+#at the command prompt
+#make
+#./target (or whatever you call the output file)
+#for use with c++ files
+.SUFFIXES : .cpp
+#Compile with g++
+CC = g++
+#use -g for gnu debugger and -std= for c++11 compiling
+CXXFLAGS = -g -std=c++11
+######Change to match all .cpp files.  Do not include .h files####
+OBJS = main.o List.o
+######Change from target to name of output file
+TARGET = HW3
+######Do NOT change this...Tabs Matter!#########
+$(TARGET): $(OBJS)
+	$(CC) -o $@ $(OBJS)
+.cpp.o:
+	$(CC) -c $(CXXFLAGS) $(INCDIR) $<
+#In order to get rid of all .o files create, at the command prompt
+#make clean
+clean:
+	rm -f $(OBJS) $(TARGET) core
