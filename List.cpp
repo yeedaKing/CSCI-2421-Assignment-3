a399a7d5f40c9685f9104d9e442abfed21f98591
yeedaKing <143111017+yeedaKing@users.noreply.github.com>
2024-02-27 02:32:56 +0000

    full code [with comments]

--- List.cpp
+++ List.cpp
@@ -9,8 +9,17 @@
 #include "List.h"
 using namespace std;
 
-void List::insert(int value) {   
+void List::insert(int value) {  
+    /**
+     * Inserts a node at the end of the list
+     *
+     * @param value Data for new node
+     * @return void
+     */
+
     Node* temp = new Node(value);
+
+    // check if list is empty, update necessary pointers
     if (!headPtr->getNextPtr()) {
         headPtr->setNextPtr(temp);
     
@@ -23,7 +32,16 @@ void List::insert(int value) {
 }
 
 void List::insertAtIndex(int data, int index) {
+    /**
+     * Inserts a node at a given index, does nothing if index is out of bounds.
+     *
+     * @param data Data for new node, should a new node be inserted
+     * @param index Insert node at this index
+     * @return void
+     */
+
     if (!headPtr->getNextPtr()) {
+        // don't do anything if list is empty and index != 0
         if (index) {
             return;
         }
@@ -32,6 +50,7 @@ void List::insertAtIndex(int data, int index) {
         tailPtr->setPrevPtr(temp);
         return;
     }
+    // traverse list until correct position is found
     Node* curr = headPtr->getNextPtr();
     Node* prev = headPtr;
     while (curr && index) {
@@ -39,9 +58,11 @@ void List::insertAtIndex(int data, int index) {
         curr = curr->getNextPtr();
         index--;
     }
+    // index isn't valid if index != 0 at this point
     if (index) {
         return;
     }
+    // check if insert position is at front of list
     if (prev == headPtr) {
         Node* temp = new Node(data, curr, nullptr);
         headPtr->setNextPtr(temp);
@@ -49,10 +70,12 @@ void List::insertAtIndex(int data, int index) {
             tailPtr->setPrevPtr(temp);
         }
 
+    // check if insert position is at end of list
     } else if (!curr) {
         Node* temp = new Node(data, nullptr, prev);
         tailPtr->setPrevPtr(temp);
 
+    // insert position at middle of list
     } else {
         Node* temp = new Node(data, curr, prev);
         prev->setNextPtr(temp);
@@ -60,9 +83,18 @@ void List::insertAtIndex(int data, int index) {
     }
 }
 void List::deleteAtIndex(int index) {
+    /**
+     * Deletes the node at a given index, does nothing if index is invalid.
+     *
+     * @param index Delete node at this index
+     * @return void
+     */
+
+    // if there are no nodes in the list, do nothing
     if (!headPtr->getNextPtr()) {
         return;
     }
+    // iterate through the list until node at given index is found
     Node* curr = headPtr->getNextPtr();
     Node* prev = headPtr;
     while (curr && index) {
@@ -70,10 +102,13 @@ void List::deleteAtIndex(int index) {
         curr = curr->getNextPtr();
         index--;
     }
+    // if index is greater than list length, do nothing
     if (index || !curr) {
         return;
     }
+    // check if curr is last node
     if (tailPtr->getPrevPtr() == curr) {
+        // check if curr is also first node
         if (prev != headPtr) {
             tailPtr->setPrevPtr(prev);
 
@@ -82,6 +117,7 @@ void List::deleteAtIndex(int index) {
         }
 
     } else {
+        // check if curr is first node
         if (prev != headPtr) {
             curr->getNextPtr()->setPrevPtr(prev);
 
@@ -89,38 +125,64 @@ void List::deleteAtIndex(int index) {
             curr->getNextPtr()->setPrevPtr(nullptr);
         }
     }
+    // delete curr node
     prev->setNextPtr(curr->getNextPtr());
+    
+    // if curr is first node, no need to reassign prev pointer
     if (prev != headPtr) {
         prev->getNextPtr()->setPrevPtr(prev);
     }
 }
 
 void List::readItem(int index) {
+    /**
+     * Prints the node's data at a given index.
+     *
+     * @param index Print node's data at this index
+     * @return void
+     */
+
     Node* curr = headPtr->getNextPtr();
     while (curr && index) {
         curr = curr->getNextPtr();
         index--;
     }
+    // if curr != nullptr, index is valid
     if (curr) {
         cout << curr->getData() << endl;
     }
 }
     
 void List::reverseList() {
+    /**
+     * Reverses the list.
+     *
+     * @return void 
+     */
+
     Node* temp = headPtr;
     Node* curr = headPtr->getNextPtr();
     while (curr) {
+        // place next node as previous node, repeat until all nodes have been swapped
         temp = curr->getPrevPtr();
         curr->setPrevPtr(curr->getNextPtr());
         curr->setNextPtr(temp);
         curr = curr->getPrevPtr();
     }
+    // reassign headPtr if temp isn't nullptr
     if (temp) {
         headPtr->setNextPtr(temp->getPrevPtr());
     }
 }
 
 void List::printForward() {
+    /**
+     * Prints each node's data of the list from front to back.
+     *
+     * @return void
+     */
+
+    // set curr to first node of list and traverse through the list
     Node* curr = headPtr->getNextPtr();
     while (curr) {
         cout << curr->getData() << ' ';
@@ -130,6 +192,13 @@ void List::printForward() {
 }
 
 void List::printBackward() {
+    /**
+     * Prints each node's data of the list in reverse order.
+     *
+     * @return void
+     */
+
+    // set curr to last node of list and traverse through the list backwards
     Node* curr = tailPtr->getPrevPtr();
     while (curr) {
         cout << curr->getData() << ' ';
@@ -137,4 +206,3 @@ void List::printBackward() {
     }
     cout << endl;
 }
-
